76dd9b0ab35dd2a7488b6b45b1cbe7ad6b5a78bd
Angela Torres <aura@MacBook-Air-de-Aura.local>
2024-10-03 21:23:50 -0500

    version 002 se agragro entidades a bases de datos

--- ecomerce/config-database.sql
+++ ecomerce/config-database.sql
@@ -9,3 +9,33 @@ mysql> CREATE TABLE  books
     -> direccion VARCHAR(60),
      -> telefono INT,
     -> PRIMARY KEY(id));
+
+
+CREATE TABLE carritodecompras (
+    cartId INT AUTO_INCREMENT PRIMARY KEY,
+    productId INT,
+    cantidad INT
+    
+);
+
+CREATE TABLE Cliente (
+  clienteId INT AUTO_INCREMENT PRIMARY KEY,
+  billingAddress VARCHAR(255),
+  shippingAddress VARCHAR(255)
+);
+CREATE TABLE Pedidos (
+  pedidoId INT AUTO_INCREMENT PRIMARY KEY,
+  clienteId INT,
+  orderDate DATE,
+  status VARCHAR(50),
+  FOREIGN KEY (clienteId) REFERENCES Cliente(clienteId)
+);
+CREATE TABLE DetallesPedido (
+  detalleId INT AUTO_INCREMENT PRIMARY KEY,
+  pedidoId INT,
+  productId INT,
+  quantity INT,
+  price DECIMAL(10, 2),
+  FOREIGN KEY (pedidoId) REFERENCES Pedidos(pedidoId)
+);
+
