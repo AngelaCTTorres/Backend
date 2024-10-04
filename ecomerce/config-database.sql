5410859895ddb14a70bfb12ee12227d57c967b6f
Angela Torres <aura@MacBook-Air-de-Aura.local>
2024-10-03 21:19:59 -0500

    version 001 base de datos

---
+++ ecomerce/config-database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE ecommerce_db
+USE ecommerce_db
+CREATE TABLE usuario
+mysql> CREATE TABLE  books 
+    -> ( id INT AUTO_INCREMENT NOT NULL,
+    -> name VARCHAR(60),
+    ->email VARCHAR(60),
+    -> contraseña INT,
+    -> direccion VARCHAR(60),
+     -> telefono INT,
+    -> PRIMARY KEY(id));
